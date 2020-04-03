59144e2329db31ac05ebbf3161055150823f02d7
Bastien Gounon <45844674+bgounon@users.noreply.github.com>
2020-04-03 02:04:15 +0200

    small layout arrangements + RGB/BL on Fn

--- keyboards/xd75/keymaps/bgounon/keymap.c
+++ keyboards/xd75/keymaps/bgounon/keymap.c
@@ -11,18 +11,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_AZ] = { // AZERTY
   { FR_ESC,     FR_AMP,         FR_EACU,        FR_QUOT,        FR_APOS,        FR_LPAR,        FR_RPAR,        FR_EQUA,        FR_DLR,        FR_MINS,        FR_EGRV,        FR_UNDS,        FR_CCED,        FR_AGRV,        MO(_FN) },
-  { FR_TAB,     FR_A,           FR_Z,           FR_E,           FR_R,           FR_T,           FR_HAT,         FR_UGRV,        FR_AST,        FR_Y,           FR_U,           FR_I,           FR_O,           FR_P,           FR_RCMD },
-  { FR_CAPL,    FR_Q,           FR_S,           FR_D,           FR_F,           FR_G,           FR_HOME,        FR_ENTR,        FR_PGUP,       FR_H,           FR_J,           FR_K,           FR_L,           FR_M,           FR_ALGR },
+  { KC_MS_BTN4, FR_A,           FR_Z,           FR_E,           FR_R,           FR_T,           FR_HAT,         FR_UGRV,        FR_AST,        FR_Y,           FR_U,           FR_I,           FR_O,           FR_P,           KC_MS_BTN5 },
+  { FR_CAPL,    FR_Q,           FR_S,           FR_D,           FR_F,           FR_G,           FR_HOME,        FR_ENTR,        FR_PGUP,       FR_H,           FR_J,           FR_K,           FR_L,           FR_M,           FR_RCMD },
   { FR_LSFT,    FR_W,           FR_X,           FR_C,           FR_V,           FR_B,           FR_END,         FR_UP,          FR_PGDN,       FR_N,           FR_COMM,        FR_SCLN,        FR_COLN,        FR_EXCL,        FR_RSFT  },
-  { FR_LCTR,    FR_BSPC,        FR_DEL,         FR_LALT,        FR_SPAC,        FR_SPAC,        FR_LEFT,        FR_DOWN,        FR_RIGT,       FR_SPAC,        FR_SPAC,        FR_DOT,         FR_SLSH,        FR_LESS,        FR_RCTR },
+  { FR_LCTR,    FR_LESS,        FR_TAB,         FR_LALT,        FR_SPAC,        FR_BSPC,        FR_LEFT,        FR_DOWN,        FR_RIGT,       FR_DEL,         FR_SPAC,        FR_ALGR,         FR_SLSH,        FR_DOT,         KC_AT },
  },
 
  [_FN] = { // FUNCTION 
-  { BL_TOGG,    FR_F1,          FR_F2,          FR_F3,          FR_F4,          FR_F5,          FR_F6,          FR_F7,          FR_F8,          FR_F9,          FR_F10,         FR_F11,         FR_F12,         FR_PSCR,        FR_TRANS },
-  { BL_INC,     FR_MPRV,        FR_MPLY,        FR_MNXT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT  },
-  { BL_DEC,     FR_MVDN,        FR_MUTE,        FR_MVUP,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT  },
-  { BL_ON,      FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT   },
-  { BL_STEP,    FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        RESET },
+  { FR_EMPT,    FR_F1,          FR_F2,          FR_F3,          FR_F4,          FR_F5,          FR_F6,          FR_F7,          FR_F8,          FR_F9,          FR_F10,         FR_F11,         FR_F12,         FR_PSCR,        FR_TRANS },
+  { FR_EMPT,    FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT  },
+  { FR_EMPT,    FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT  },
+  { BL_TOGG,    BL_STEP,        BL_BRTG,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT,        FR_EMPT   },
+  { RGB_TOG,    RGB_RMOD,       RGB_MOD,        RGB_HUD,        RGB_HUI,        RGB_SAD,        RGB_SAI,        RGB_VAD,        RGB_VAI,        RGB_SPD,        RGB_SPI,        FR_EMPT,        FR_EMPT,        FR_EMPT,        RESET },
  },
 
 };
